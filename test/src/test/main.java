220e3a94c6720111fbf7c97651440745280ac35d
yukiaqua <s15505@salesio-sp.ac.jp>
2019-04-25 00:57:18 +0900

    なおした

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -1,27 +1,25 @@
 package test;
 
 public class main {
-
-	private String bookname;
-	private int cost;
-	private int code;
-
-	public String bookname(){
-		return bookname;
-	}
-	public int cost(){
-		return cost;
-	}
-	public int code(){
-		return code;
-	}
+	main Book = new main();
+	Book.bookname = "赤軍に勝るものなし";
+	Book.cost = 1922;
+	Book.code = 19221991;
 
 	public static void main(String[] args) {
-		main Book = new main();
-		Book.bookname = "赤軍に勝るものなし";
-		Book.cost = 1922;
-		Book.code = 19221991;
+		private String bookname;
+		private int cost;
+		private int code;
 
+		public String bookname(){
+			return bookname;
+		}
+		public int cost(){
+			return cost;
+		}
+		public int code(){
+			return code;
+		}
 	}
 }
 
