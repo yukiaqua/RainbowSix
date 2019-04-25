41e622992624d20e96ad4c687a28f3426658fa76
yukiaqua <s15505@salesio-sp.ac.jp>
2019-04-25 23:21:11 +0900

    書籍情報を戻り値として表示するよう改変

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -1,25 +1,30 @@
 package test;
 
-public class main {
-	main Book = new main();
-	Book.bookname = "赤軍に勝るものなし";
-	Book.cost = 1922;
-	Book.code = 19221991;
+public class book {
+	private String bookname = "赤軍に勝るものなし";
+	private int cost = 1922;
+	private int code = 19221991;
 
-	public static void main(String[] args) {
-		private String bookname;
-		private int cost;
-		private int code;
-
-		public String bookname(){
-			return bookname;
-		}
-		public int cost(){
-			return cost;
-		}
-		public int code(){
-			return code;
-		}
+	public String A(){
+		return bookname;
+	}
+	public int B(){
+		return cost;
+	}
+	public int C(){
+		return code;
 	}
 }
+
+public class Main {
+	public static void main(String[] args) {
+	book Book = new book();
+
+    System.out.println(Book.A());
+    System.out.println(Book.B());
+    System.out.println(Book.C());
+
+	}
+}
+
 
